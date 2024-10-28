1ba36c58fd1cfedf4b1010a2766eef78015433a7
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-10-28 00:59:29 -0400

    another quick fix

--- src/ProjectThumbnail/ProjectThumbnailTemplate.tsx
+++ src/ProjectThumbnail/ProjectThumbnailTemplate.tsx
@@ -15,7 +15,7 @@ export default function ProjectThumbnailTemplate(props:{
         <Link to={"/pages-test/"+props.id}>
 
             <div className={"book"}>
-                {<img src={"/pages-test/src/assets/ProjectsAssets/gl2.png"} className={"bookGold"} alt={"gold"}/>}
+                <img src={"/pages-test/src/assets/ProjectsAssets/gl2.png"} className={"bookGold"} alt={"gold"}/>
                 <img src={"/assets/ProjectsAssets/bl4.png"} className={"bookCover"} alt={"cover"} id={props.id}/>
                 <div className={"topSection"}>
                     <p className={"titleText"}>{props.title}</p>
