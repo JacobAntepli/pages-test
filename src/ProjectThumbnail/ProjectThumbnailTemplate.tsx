7684ee653bd68457b485d17db95b9d66670cccdb
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-10-28 00:50:10 -0400

    more image testing

--- src/ProjectThumbnail/ProjectThumbnailTemplate.tsx
+++ src/ProjectThumbnail/ProjectThumbnailTemplate.tsx
@@ -12,15 +12,15 @@ export default function ProjectThumbnailTemplate(props:{
 
     return(
         <Link to={"/pages-test/"+props.id}>
-            
-            <div className={"book"} >
-                <img src={"src/assets/ProjectsAssets/gl2.png"} className={"bookGold"} alt={"gold"} />
-                <img src={"src/assets/ProjectsAssets/bl4.png"} className={"bookCover"} alt={"cover"} id={props.id}/>
-                        <div className={"topSection"}>
-                            <p className={"titleText"}>{props.title}</p>
-                            <img className={"thumbnail"} src={props.imageURL} alt={"Title"}/>
-                            <p className={"toolsText"}>{props.tools}</p>
-                        </div>
+
+            <div className={"book"}>
+                <img src={"/pages-test/assets/ProjectsAssets/gl2.png"} className={"bookGold"} alt={"gold"}/>
+                <img src={"/assets/ProjectsAssets/bl4.png"} className={"bookCover"} alt={"cover"} id={props.id}/>
+                <div className={"topSection"}>
+                    <p className={"titleText"}>{props.title}</p>
+                    <img className={"thumbnail"} src={props.imageURL} alt={"Title"}/>
+                    <p className={"toolsText"}>{props.tools}</p>
+                </div>
                 <div className={"bottomSection"}>
                     <p className={"summaryHeader"}>Summary:</p>
                     <p className={"summaryText"}>{props.summary}</p>
